938181ec307f1265d6ec09a274060e404134bea6
tilak <tilakas13@gmail.com>
2021-03-12 20:01:57 +0530

    initial commit

---
+++ app/src/main/java/com/tilaka/apps/architecture/components/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tilaka.apps.architecture.components.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tilaka.apps.architecture.components.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
